fc7d3ec84bf02aac4dd7f5b6c9bd80fa56eaf6fd
Jack Hong <jack.xu.hong@gmail.com>
2020-05-17 12:02:14 -0400

    Added some test functions

--- gcauto.py
+++ gcauto.py
@@ -1,12 +1,12 @@
 import functools
 import json
 import os
+from datetime import datetime
 
 import flask
-
-from authlib.client import OAuth2Session
 import google.oauth2.credentials
 import googleapiclient.discovery
+from authlib.integrations.requests_client import OAuth2Session
 from googleapiclient.discovery import build
 
 import google_auth
@@ -18,14 +18,33 @@ app.config['SESSION_TYPE'] = 'filesystem'
 app.register_blueprint(google_auth.app)
 
 
+def gc_service():
+    credentials = google_auth.build_credentials()
+    service = build('classroom', 'v1', credentials=credentials)
+    return service
+
+
+def test_task1(courseId):
+    today = datetime.today().strftime('%Y-%m-%d')
+    daily_workout = {
+        'title': f'Daily Workout {today}',
+        'description': 'Healthy body healthy brain!',
+        'maxPoints': 30,
+        'workType': 'ASSIGNMENT',
+        'state': 'PUBLISHED'
+    }
+    daily_workout = gc_service().courses().courseWork().create(
+        courseId=courseId, body=daily_workout).execute()
+
+
 @app.route('/')
 def index():
     if google_auth.is_logged_in():
         user_info = google_auth.get_user_info()
 
-        credentials = google_auth.build_credentials()
-        service = build('classroom', 'v1', credentials=credentials)
-        results = service.courses().list(pageSize=10).execute()
+        #credentials = google_auth.build_credentials()
+        #service = build('classroom', 'v1', credentials=credentials)
+        results = gc_service().courses().list(pageSize=10).execute()
         courses = results.get('courses', [])
         print(courses)
 
